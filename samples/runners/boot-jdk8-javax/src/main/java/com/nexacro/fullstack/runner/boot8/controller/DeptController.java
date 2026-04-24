060a27c3260b7fc683466b10f6dd39a545d210f6
Mason More <cubists@gmail.com>
2026-04-24 09:10:52 +0900

    feat(runner-boot-jdk8-javax): add DeptController

---
+++ samples/runners/boot-jdk8-javax/src/main/java/com/nexacro/fullstack/runner/boot8/controller/DeptController.java
@@ -0,0 +1,30 @@
+package com.nexacro.fullstack.runner.boot8.controller;
+
+import com.nexacro.fullstack.business.domain.dept.DeptService;
+import com.nexacro.fullstack.business.uiadapter.NexacroController;
+import com.nexacro.fullstack.business.uiadapter.NexacroResponseBuilder;
+import com.nexacro.fullstack.business.xapi.NexacroEnvelope;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dept")
+public class DeptController extends NexacroController {
+
+    private final DeptService deptService;
+
+    public DeptController(DeptService deptService) {
+        this.deptService = deptService;
+    }
+
+    @PostMapping("/list.do")
+    public NexacroEnvelope list() {
+        return NexacroResponseBuilder.ok(deptService.listAll());
+    }
+
+    @PostMapping("/tree.do")
+    public NexacroEnvelope tree() {
+        return NexacroResponseBuilder.ok(deptService.tree());
+    }
+}
