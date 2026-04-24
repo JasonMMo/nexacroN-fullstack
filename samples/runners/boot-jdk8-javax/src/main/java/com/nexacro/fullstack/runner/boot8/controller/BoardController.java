8a2cd87c6ce2a7d86c270c6f961d6f417762366f
Mason More <cubists@gmail.com>
2026-04-24 09:10:44 +0900

    feat(runner-boot-jdk8-javax): add BoardController

---
+++ samples/runners/boot-jdk8-javax/src/main/java/com/nexacro/fullstack/runner/boot8/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.nexacro.fullstack.runner.boot8.controller;
+
+import com.nexacro.fullstack.business.domain.board.BoardService;
+import com.nexacro.fullstack.business.uiadapter.NexacroController;
+import com.nexacro.fullstack.business.uiadapter.NexacroResponseBuilder;
+import com.nexacro.fullstack.business.xapi.NexacroDataset;
+import com.nexacro.fullstack.business.xapi.NexacroEnvelope;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample/board")
+public class BoardController extends NexacroController {
+
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @PostMapping("/select.do")
+    public NexacroEnvelope select(@RequestBody(required = false) NexacroEnvelope req) {
+        return NexacroResponseBuilder.ok(boardService.selectAll());
+    }
+
+    @PostMapping({"/insert.do", "/update.do", "/delete.do"})
+    public NexacroEnvelope mutate(@RequestBody NexacroEnvelope req) {
+        NexacroDataset input = datasetById(req, "input");
+        int affected = boardService.processRows(input);
+        NexacroEnvelope out = NexacroResponseBuilder.ok();
+        out.getParameters().add(new NexacroEnvelope.Parameter("affectedRows", affected, "int"));
+        return out;
+    }
+}
